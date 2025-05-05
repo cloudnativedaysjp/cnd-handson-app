40dfb610b43ff6898d213e1af3a9d12b890d5ecf
Tatsuki Minami <mtatsu10@gmail.com>
2025-05-05 18:48:33 +0000

    protoの生成

--- backend/board/src/main.ts
+++ backend/board/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // gRPCのオプションを設定
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'user',
+      protoPath: join(__dirname, './proto/board.proto'),
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
 }
 bootstrap();
